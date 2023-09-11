987358d6f997f7b4156ec0652e46c579e52434a2
Olga <you@example.com>
2023-09-11 13:58:42 +0300

    кодинг на JS

---
+++ script.js
@@ -0,0 +1,79 @@
+//Задание №1
+
+let a=Number(10);
+alert(a);
+a=Number(20);
+alert(a)
+
+//Задание №2
+
+let firstIphone=number(2007);
+alert(`год выпуска первого Iphone: ${firstIphone} `);
+
+//Задание №3
+let autorJs=String(`Брендан Эйх`);
+alert(`Имя создателя языка JS: ${autorJs}`);
+
+//Задание №4
+let d=Number(10);
+let b=Number(2);
+let sum=(d+c);
+alert(`Сумма: ${sum}`);
+let difference=(d-b);
+alert (`Разность: &{difference}`);
+let multiplication=(d*b);
+alert(`Умножение: ${multiplication}`);
+let division=(d/b);
+alert (`деление: ${divisio}`);
+
+//Задание №5
+let resilt=(2**5);
+alert(`Возведение 2 в 5-ю степень: &{result}`);
+
+//Задание №6
+let a=Number(9);
+let b=Number(2);
+let divide=(a%b);
+alert(`Остаток от деления: ${divide}`);
+
+//Задание №7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+
+num = ++num ;
+num = --num;
+alert(num);
+
+//Задание №8
+let age=prompt(`Сколько вам лет`);
+alert(age);
+
+//Задание №9.0
+const user={name:`Olga`,age:34,isAdmin:true}
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin);
+
+//Задание №9.1
+user[`city of residence`]=true;
+alert([`city of residence`]);
+
+//Задание №9.2
+user.age=34;
+alert(user.age);
+
+//Задание №9.3
+delete user[`city of residence`];
+
+//Задание №9.4
+let info=prompt(`Какую информацию хотите узнать о пользователе`);
+alert(user[info]);
+
+//Задание №10
+let Name=prompt(`Ваше имя?`);
+alert(`Привет, ${Name}!`);
+
+
